71c41b6612a81b6ecbe23fb6d76c7343ba5fd15d
liujf <q1463303508@163.com>
2018-07-09 20:08:03 +0800

    完成所有

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var scss = require('gulp-clean-css');
 var babel = require('gulp-babel');
 var proData = require('./data/procuct.json');
 var html = require('gulp-htmlmin');
+var rev = require('gulp-rev');
+var colle = require('gulp-rev-collector');
 // 起服务
 gulp.task('server', function() {
     gulp.src('src')
@@ -32,26 +34,35 @@ gulp.task('server', function() {
 });
 //js
 gulp.task('uglify', function() {
-    gulp.src('./src/js/*.js')
+    gulp.src('./src/js/**/*.js')
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest(`.sr/js`))
+        .pipe(rev())
+        .pipe(gulp.dest(`build/js`))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/js'))
 });
 // 压缩css
-// gulp.task('scss', function() {
-//     gulp.src('./src/css/*.css')
-//         .pipe(scss())
-//         .pipe(gulp.dest('./build/css'))
-// });
+gulp.task('scss', function() {
+    gulp.src('./src/css/*.css')
+        .pipe(scss())
+        .pipe(rev())
+        .pipe(gulp.dest('./build/css'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/css'))
+});
 // 压缩html
-// gulp.task('html', function() {
-//     gulp.src('./src/**/*.html')
-//         .pipe(html({
-//             collapseWhitespace: true,
-//             removeComments: true
-//         }))
-//         .pipe(gulp.dest('./build'))
-// });
-// gulp.task('all', ['uglify', 'scss', 'html'])
+gulp.task('html', function() {
+    gulp.src('./src/**/*.html')
+        .pipe(html({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(rev())
+        .pipe(gulp.dest('./build'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/html'))
+});
+gulp.task('all', ['uglify', 'scss', 'html'])
